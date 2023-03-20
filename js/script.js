829c604326f708b594515209eb4f9b0d0696e13a
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-03-20 22:14:00 +0000

    built website and added address finder

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Mohamad All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohamad
+// Created on: Sep 2023
 // This file contains the JS functions for index.html
+
+/*
+* This function collects the user's input and displays a combined result.
+*/
+function buttonClicked() {
+  // input
+  const streetName = document.getElementById('street-name').value
+  const streetNumber = parseInt(document.getElementById('street-number').value)
+
+  // output
+  document.getElementById('address').innerHTML = 
+    "Your address is: " + streetName + " " + streetNumber + "."
+}
