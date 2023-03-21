8e157ce48df3864aab099c88a5b04184e376053f
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-03-21 16:41:05 +0000

    added favicon

---
+++ go_app/main.go
@@ -0,0 +1,29 @@
+// Created by: Mohamad Tanbari
+// Created on: Mar 2023
+//
+// This program displays, "Hello, World!"
+
+package main
+
+import "fmt"
+
+func main() {
+	// This function showws your adress
+	var streetName string
+	var streetNumber int
+
+	// input
+	fmt.Println("This program shows your address.")
+	fmt.Println()
+	fmt.Print("Enter your street number: ")
+	fmt.Scanln(&streetNumber)
+	fmt.Print("Enter your street name: ")
+	fmt.Scanln(&streetName)
+
+	// output
+	fmt.Println("Your address is", streetNumber, streetName, "Street.")
+
+	// Done
+	fmt.Println("\nDone.")
+
+}
